1eec788967597b4e51c6bf057802b6193514a9d9
raptorattack <brockmeier.adam@gmail.com>
2019-02-02 20:44:31 -0500

    fuck this

---
+++ Phildo/cppFiles/example.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+#include "functions.h"
+
+using namespace std;
+
+void printtoscreen() {
+
+	for (int i = 0; i < 20; i++) {
+
+		cout << "hello w0rld" << endl;
+
+	}
+
+
+
+}
+
--- Phildo/justToCodeProject/build/cmake_install.cmake
+++ Phildo/justToCodeProject/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/philip.fritz/Documents/RatJarStudios/Phildo/justToCodeProject
+# Install script for directory: C:/GIT_ROOT/RatJarStudios/Phildo/justToCodeProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/example.cpp")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/exampleMain.cpp")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/philip.fritz/Documents/RatJarStudios/Phildo/justToCodeProject/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/GIT_ROOT/RatJarStudios/Phildo/justToCodeProject/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
